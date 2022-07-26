28165fdbefee6dc8766b1635d2f5a823f80a5e95
Rashid Gaitov <rashidgaitov@gmail.com>
2022-07-26 15:17:34 +0300

    Initial Commit

---
+++ VideoGallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VideoGallery
+//
+//  Created by Rashid Gaitov on 26.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
